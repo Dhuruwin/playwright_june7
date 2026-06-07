830bba76a52938774da7e4919e32356f487d1080
Dhuruwin R <dhuruwinraja@gmail.com>
2026-06-07 18:35:26 +0530

    new updated

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,4 +6,5 @@ test('login test',async({page})=>{
    const user1 = new Loginpage(page)
    await user1.Navigate()
    await user1.loginpage()
+   await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
 });
